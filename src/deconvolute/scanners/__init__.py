888ae9033803dd3f062359be4a976e697d08f2cb
daved01 <54682095+daved01@users.noreply.github.com>
2026-02-22 11:59:30 +0100

    chore: fix exports

--- src/deconvolute/scanners/__init__.py
+++ src/deconvolute/scanners/__init__.py
@@ -3,6 +3,7 @@ from deconvolute.models.security import SecurityResult
 from .base import BaseScanner
 from .content.language.engine import LanguageScanner
 from .content.language.models import LanguageSecurityResult
+from .content.signature.engine import SignatureScanner
 from .integrity.canary.engine import CanaryScanner
 from .integrity.canary.models import CanarySecurityResult
 
@@ -13,4 +14,5 @@ __all__ = [
     "LanguageScanner",
     "LanguageSecurityResult",
     "SecurityResult",
+    "SignatureScanner",
 ]
